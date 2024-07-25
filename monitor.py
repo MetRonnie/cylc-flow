3f3b21c737f48ae68509222be007f883d972312a
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2024-07-25 15:46:45 +0100

    Per-cpu monitor

--- monitor.py
+++ monitor.py
@@ -5,9 +5,13 @@ from datetime import datetime
 def monitor():
     with open('cpu.txt', 'a') as f:
         while True:
-            cpu = psutil.cpu_percent(interval=2)
-            tstamp = datetime.isoformat(datetime.now(), timespec='seconds')
-            print(f"{tstamp} {round(cpu) * '|'} ({cpu}%)", file=f)
+            cpus = psutil.cpu_percent(interval=2, percpu=True)
+            tstamp = datetime.now().strftime('%H:%M:%S')
+            bars = (
+                f"{round(cpu / 4) * '|'}{(25 - round(cpu / 4)) * ' '}%"
+                for cpu in cpus
+            )
+            print(f"{tstamp} {' '.join(bars)} {cpus}", file=f)
             f.flush()
 
 
